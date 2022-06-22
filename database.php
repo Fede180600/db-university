b7e7baaec16d4bf6ace83502e2e6817f43d2df93
Fede180600 <fede180600@libero.it>
2022-06-22 15:39:57 +0200

    database name and password

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php 
     define("DB_SERVERNAME", "localhost");
     define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "");
-    define("DB_NAME", "university_db");
+    define("DB_PASSWORD", "root");
+    define("DB_NAME", "university-db");
     define("DB_PORT", 3306);
 
     $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
